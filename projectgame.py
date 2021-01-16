06940a84a835f49fddf050991e4b388013044b25
ArtemStepNkey <a.stepanov200552@gmail.com>
2021-01-16 13:44:43 +0300

    рисуется квадратик

--- projectgame.py
+++ projectgame.py
@@ -3,19 +3,20 @@ from pygame import *
 import sys
 import os
 
-WIDTH = 1000
+WIDTH = 992
 HEIGHT = 700
 DISPLAY = (WIDTH, HEIGHT)
 
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 
 FPS = 60
-
+step = 50
 
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
@@ -30,48 +31,98 @@ screen.fill(BLACK)
 pygame.display.flip()
 
 level = [
-       "-------------------------",
-       "-                       -",
-       "-                       -",
-       "-                       -",
-       "-            --         -",
-       "-                       -",
-       "--                      -",
-       "-                       -",
-       "-                   --- -",
-       "-                       -",
-       "-                       -",
-       "-      ---              -",
-       "-                       -",
-       "-   -----------        -",
-       "-                       -",
-       "-                -      -",
-       "-                   --  -",
-       "-                       -",
-       "-                       -",
-       "-------------------------"]
+       "-------------------------------",
+       "-                             -",
+       "-                             -",
+       "-                             -",
+       "-            --               -",
+       "-                             -",
+       "--                            -",
+       "-                             -",
+       "-                   ---       -",
+       "-                             -",
+       "-                             -",
+       "-      ---                    -",
+       "-                             -",
+       "-   -----------               -",
+       "-                             -",
+       "-                -            -",
+       "-                   --        -",
+       "-                             -",
+       "-                             -",
+       "-                             -",
+       "-                             -",
+       "-------------------------------"]
 
-x = y = 0  # координаты
-for row in level:  # вся строка
-    for col in row:  # каждый символ
+x = y = 0
+for row in level:
+    for col in row:
         if col == "-":
-            # создаем блок, заливаем его цветом и рисеум его
             pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
             pf.fill(WHITE)
             screen.blit(pf, (x, y))
             display.update()
 
-        x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
-    y += PLATFORM_HEIGHT  # то же самое и с высотой
-    x = 0  # на каждой новой строчке начинаем с нуля
+        x += PLATFORM_WIDTH
+    y += PLATFORM_HEIGHT
+    x = 0
 
+MOVE_SPEED = 7
+WIDTH_P = 22
+HEIGHT_P = 32
+
+left = right = False
+
+class Player(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.xvel = 0
+        self.startX = x
+        self.startY = y
+        self.image = Surface((WIDTH_P, HEIGHT_P))
+        self.image.fill(BLUE)
+
+        self.rect = Rect(x, y, WIDTH_P, HEIGHT_P)
+
+    def update(self, left, right):
+        if left:
+            self.xvel = -MOVE_SPEED  # Лево = x- n
+
+        if right:
+            self.xvel = MOVE_SPEED  # Право = x + n
+
+        if not (left or right):  # стоим, когда нет указаний идти
+            self.xvel = 0
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+
+    def draw(self, screen):  # Выводим себя на экран
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+hero = Player(55, 55)
 
 
 
 running = True
 while running:
     clock.tick(FPS)
+    x = y = 0
+    hero.draw(screen)
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == KEYDOWN and event.key == K_LEFT:
+            left = True
+        elif event.type == KEYDOWN and event.key == K_RIGHT:
+            right = True
+
+        elif event.type == KEYUP and event.key == K_RIGHT:
+            right = False
+        elif event.type == KEYUP and event.key == K_LEFT:
+            left = False
+    hero.update(left, right)  # передвижение
+      # отображение
+    display.update()
+
 pygame.quit()
