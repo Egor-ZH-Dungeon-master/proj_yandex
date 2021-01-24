a3aba155cdc9461bea50ca901595ca864da6f7c8
Egor <77405682+Egor-ZH-Dungeon-master@users.noreply.github.com>
2021-01-24 22:25:26 +0300

    Первый комит

--- projectgame.py
+++ projectgame.py
@@ -2,17 +2,19 @@ import pygame
 import random
 import os
 
-
 pygame.init()
-
+# размеры игры
 WIDTH = 1000
 HEIGHT = 750
 
+# ограничение кадров
 fps = 100
 clock = pygame.time.Clock()
 
+# создание дисплея
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# название игры
 pygame.display.set_caption("Лягуха прыгуха :)")
 pygame.mixer.music.load('data/fonpesn.mp3')
 pygame.mixer.music.set_volume(0.3)
@@ -23,35 +25,28 @@ proigral = pygame.mixer.Sound('data/proigral.wav')
 plusserd = pygame.mixer.Sound('data/plusserd.wav')
 click = pygame.mixer.Sound('data/click.wav')
 
-
+# иконка игры
 ikonka = pygame.image.load("data/lyag0.png")
 pygame.display.set_icon(ikonka)
-
-
+# кактусы
 cac_im = [pygame.image.load("data/Cactus0.png"), pygame.image.load("data/Cactus1.png"),
           pygame.image.load("data/Cactus2.png")]
 cac_opt = [69, 573, 37, 534, 40, 544]
 
-
+# определение персонажа
 pers_im = [pygame.image.load("data/lyag2.png"), pygame.image.load("data/lyag1.png"),
            pygame.image.load("data/lyag0.png")]
 im_counter = 0
 
-
 stone_im = [pygame.image.load("data/Stone0.png"), pygame.image.load("data/Stone1.png")]
 
-
+# облака
 cloud_im = [pygame.image.load("data/Cloud0.png"), pygame.image.load("data/Cloud1.png")]
-
 
 scores = 0
 max_score = 0
-
-
 max_cact = 0
 nad_cactus = False
-
-
 health = 2
 health_im = pygame.image.load('data/serd.png')
 health_im = pygame.transform.scale(health_im, (30, 30))
@@ -61,7 +56,7 @@ def s_men():
     men_im = pygame.image.load("data/fon.jpg")
     show = True
 
-    start_b = Button(260, 70)
+    start_b = Button(305, 70)
 
     while show:
         for event in pygame.event.get():
@@ -70,7 +65,7 @@ def s_men():
                 quit()
 
         display.blit(men_im, (0, 0))
-        start_b.draw(350, 400, "ПЕРЕЙТИ В ИГРУ!", s_game, 50)
+        start_b.draw(350, 400, "ПЕРЕЙТИ В CUMZONE!", s_game, 50)
 
         pygame.display.update()
         clock.tick(60)
@@ -136,22 +131,26 @@ class Button:
         p_text(message, x + 10, x + 60)
 
 
-user_WIDTH = 70
-user_HEIGHT = 60
+user_WIDTH = 80
+user_HEIGHT = 76
 
+# положение персонажа
 user_x = WIDTH // 4
 user_y = HEIGHT - user_HEIGHT - 126
 
 m_jump = False
 c_jump = 30
 
+# размеры препятствия
 cac_WIDTH = 25
 cac_HEIGHT = 80
 
+# положение препятствия
 cac_x = WIDTH - 40
 cac_y = HEIGHT - cac_HEIGHT - 80
 
 
+# сам игровой цикл
 def game_1():
     global m_jump
     pygame.mixer.music.play(-1)
@@ -473,7 +472,6 @@ def plus_h(heart):
 
         radius = WIDTH + random.randrange(500, 1700)
         heart.ret_self(radius, heart.y, heart.width, heart.image)
-
 
 s_men()
 
